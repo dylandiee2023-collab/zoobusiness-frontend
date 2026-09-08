b84d276351ae1e3a8ec671b2924e971576b9007c
dylandiee2023-collab <dylandiee2023@gmail.com>
2026-09-08 11:16:49 +0300

    chore(frontend): remove duplicate app providers

--- src/app/providers/AppProviders.tsx
+++
@@ -1,7 +0,0 @@
-import type { PropsWithChildren } from "react";
-
-import { ThemeProvider } from "@/theme/providers";
-
-export function AppProviders({ children }: PropsWithChildren) {
-  return <ThemeProvider>{children}</ThemeProvider>;
-}

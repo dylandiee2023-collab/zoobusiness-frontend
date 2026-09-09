90c144534b346a94f1121b56816346711c405ca0
dylandiee2023-collab <dylandiee2023@gmail.com>
2026-09-09 16:04:27 +0300

    fix(ds): prevent input layout props leaking to DOM

--- src/design-system/inputs/Input.tsx
+++ src/design-system/inputs/Input.tsx
@@ -7,17 +7,22 @@ import { inputRecipe } from "./Input.recipe";
 import type { InputProps } from "./Input.types";
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(function Input(
-  { style, ...props },
+  { style, fullWidth, invalid, ...props },
   ref,
 ) {
   const { theme } = useTheme();
 
-  const recipe = inputRecipe(theme, props);
+  const recipe = inputRecipe(theme, {
+    ...props,
+    fullWidth,
+    invalid,
+  });
 
   return (
     <input
       ref={ref}
       {...props}
+      aria-invalid={invalid || undefined}
       style={{
         ...recipe.style,
         ...style,

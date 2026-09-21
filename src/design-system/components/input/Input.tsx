83db0d883ae7d7b2bcf709984f955d8e0e1d389d
dylandiee2023-collab <dylandiee2023@gmail.com>
2026-09-21 19:37:17 +0300

    refactor: route duplicate input component to canonical implementation

--- src/design-system/components/input/Input.tsx
+++ src/design-system/components/input/Input.tsx
@@ -1,15 +1,1 @@
-import type { JSX } from "react";
-
-import { inputRecipe } from "./Input.recipe";
-import type { InputProps } from "./Input.types";
-
-export function Input({
-  size = "md",
-  error = false,
-  className = "",
-  ...props
-}: InputProps): JSX.Element {
-  return (
-    <input className={`${inputRecipe(size, error)} ${className}`} {...props} />
-  );
-}
+export { Input } from "../../inputs";
